65cb2900a5bf6e01d00bf138c2a2800ca16fd88a
Johan Rong <johanhrong@icloud.com>
2026-09-26 15:21:52 +0200

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
     const t = useTranslations("footer")
 
     return (
-        <footer className="flex w-full py-12 justify-center mt-auto">
+        <div className="flex w-full py-12 justify-center mt-auto">
             <div className="flex w-4xl max-w-4xl justify-between">
                 <div className="flex flex-col-reverse">
                     <Text size="text-md">
@@ -28,6 +28,6 @@ export function Footer() {
                     </Link>
                 </div>
             </div>
-        </footer>
+        </div>
     )
 }
